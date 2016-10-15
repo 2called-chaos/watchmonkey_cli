d635ae334c21d7c87e519987082cfaa4f3725c28
Sven Pachnit <sven@bmonkeys.net>
2016-10-16 01:49:15 +0200

    Fix unix_defaults

--- lib/watchmonkey_cli/checkers/unix_defaults.rb
+++ lib/watchmonkey_cli/checkers/unix_defaults.rb
@@ -8,11 +8,11 @@ module WatchmonkeyCli
           opts = { unix_load: {}, unix_memory: {}, unix_df: {}, unix_mdadm: {} }.merge(opts)
 
           # option shortcuts
-          opts[:unix_load][:limits] = opts[:load]
+          opts[:unix_load][:limits] = opts[:load] if opts[:load]
           opts[:unix_load] = false if opts[:load] == false
-          opts[:unix_memory][:min_percent] = opts[:memory_min]
+          opts[:unix_memory][:min_percent] = opts[:memory_min] if opts[:memory_min]
           opts[:unix_memory] = false if opts[:memory_min] == false
-          opts[:unix_df][:min_percent] = opts[:df_min]
+          opts[:unix_df][:min_percent] = opts[:df_min] if opts[:df_min]
           opts[:unix_df] = false if opts[:df_min] == false
           opts[:unix_mdadm] = false if opts[:mdadm] == false
 
